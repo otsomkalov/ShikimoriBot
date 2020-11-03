9ddc48ef21e9cded4a791259d12927ca8f9793ac
Oleg Tsomkalov <infinitu1327@gmail.com>
2020-11-03 12:41:48 +0200

    Add handling of empty inline query & answer query exception handling

--- ShikimoriTelegramBot/Services/InlineQueryService.cs
+++ ShikimoriTelegramBot/Services/InlineQueryService.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using ShikimoriNET;
 using ShikimoriNET.Params.Anime;
 using ShikimoriTelegramBot.Helpers;
@@ -13,26 +15,36 @@ namespace ShikimoriTelegramBot.Services
     {
         private readonly ShikimoriApi _api;
         private readonly ITelegramBotClient _bot;
+        private readonly ILogger<InlineQueryService> _logger;
 
-        public InlineQueryService(ITelegramBotClient bot, ShikimoriApi api)
+        public InlineQueryService(ITelegramBotClient bot, ShikimoriApi api, ILogger<InlineQueryService> logger)
         {
             _bot = bot;
             _api = api;
+            _logger = logger;
         }
 
         public async Task HandleAsync(InlineQuery inlineQuery)
         {
-            // _logger.Information("Got inline query {@InlineQuery}", inlineQuery);
-
-            var animes = await _api.Anime.SearchAsync(new SearchParams
+            if (!string.IsNullOrEmpty(inlineQuery.Query))
             {
-                Search = inlineQuery.Query,
-                Limit = 10
-            });
+                var animes = await _api.Anime.SearchAsync(new SearchParams
+                {
+                    Search = inlineQuery.Query,
+                    Limit = 10
+                });
 
-            var response = animes.Select(InlineQueryResultArticleHelpers.GetInlineQueryResultArticleForAnime);
+                var response = animes.Select(InlineQueryResultArticleHelpers.GetInlineQueryResultArticleForAnime);
 
-            await _bot.AnswerInlineQueryAsync(inlineQuery.Id, response);
+                try
+                {
+                    await _bot.AnswerInlineQueryAsync(inlineQuery.Id, response);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Error during AnswerInlineQueryAsync");
+                }
+            }
         }
     }
 }
